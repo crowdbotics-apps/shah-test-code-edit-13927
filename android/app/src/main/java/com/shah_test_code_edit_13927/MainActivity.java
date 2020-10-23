c9f1f33822f591681f997dc903f1801719ef366a
Crowdbotics <team@crowdbotics.com>
2020-10-23 12:41:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shah_test_code_edit_13927/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shah_test_code_edit_13927;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "shah_test_code_edit_13927";
+    }
+}
---
+++ ios/shah_test_code_edit_13927Tests/shah_test_code_edit_13927Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface shah_test_code_edit_13927 : XCTestCase
+
+@end
+
+@implementation shah_test_code_edit_13927
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
